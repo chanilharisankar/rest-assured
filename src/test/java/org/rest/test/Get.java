7d6d2beeac5da618fb9aa1654cfcf5708e68cac9
haris <haris@in-haris.local>
2017-08-17 12:15:42 +0530

    simple test

--- src/test/java/org/rest/test/Get.java
+++ src/test/java/org/rest/test/Get.java
@@ -1,7 +1,13 @@
 package org.rest.test;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by haris on 17/08/17.
  */
 public class Get {
+    @Test
+    public void getTest(){
+        System.out.print("yes");
+    }
 }
