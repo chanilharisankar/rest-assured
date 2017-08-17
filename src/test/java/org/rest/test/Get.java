a59fb1b9b9db2a449d82a4c23134d97dbc7cb9e5
haris <haris@in-haris.local>
2017-08-17 16:08:11 +0530

    test

--- src/test/java/org/rest/test/Get.java
+++ src/test/java/org/rest/test/Get.java
@@ -1,6 +1,8 @@
 package org.rest.test;
 
+
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 /**
  * Created by haris on 17/08/17.
@@ -9,5 +11,6 @@ public class Get {
     @Test
     public void getTest(){
         System.out.print("yes");
+        Assert.assertEquals("Google","");
     }
 }
