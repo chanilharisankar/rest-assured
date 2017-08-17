d2c64fe719e1fd82488f8deef381d56007e6a208
haris <haris@in-haris.local>
2017-08-17 22:08:00 +0530

    get

--- src/test/java/org/rest/test/Get.java
+++ src/test/java/org/rest/test/Get.java
@@ -4,6 +4,11 @@ package org.rest.test;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
+import static io.restassured.RestAssured.registerParser;
+import static io.restassured.RestAssured.unregisterParser;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by haris on 17/08/17.
  */
@@ -11,6 +16,10 @@ public class Get {
     @Test
     public void getTest(){
         System.out.print("yes");
-        Assert.assertEquals("Google","");
+        when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("page", is(2));
     }
 }
